9838d82e5a8308486e6ddfa6b501632d9707176a
Alex Alvarado <Alexjalva@gmail.com>
2021-04-29 21:29:51 -0700

    added display functionality

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,10 +1,44 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
 function ResultList(props) {
+  
   return (
-    <ul className="list-group">
-      {console.log(props.results)}
-    </ul>
+    <table style={{width: "100%"}}>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Phone Number</th>
+          <th>Email</th>
+          <th>Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map(employee => 
+          <tr style = {{borderTop: "5px dashed black"}}>
+            <td>
+              <img src={employee.picture.thumbnail} />
+            </td>
+            <td>
+              {employee.name.first}{employee.name.last}
+            </td>
+            <td>
+              {employee.phone}
+            </td>
+            <td>
+              {employee.email}
+            </td>
+            <td>
+              {employee.location.state},{employee.location.country}
+            </td>
+          </tr>)}
+      </tbody>
+    </table>
+    // <ul className="list-group">
+
+    //   {console.log(props.results)}
+    // </ul>
   );
 }
 
